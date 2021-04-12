3d626390202a7d83af8bc31b7a0ed1a33444ed2a
111 <a@1>
2021-04-12 20:22:54 +0300

    test

--- test.cpp
+++ test.cpp
@@ -1,10 +1,56 @@
-#pragma once
+#include "histogram.h"
+#include <cassert>
 
-#include <iostream>
-#include <vector>
-using namespace std;
+void test_positive()//Случай массива полож. чисел
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
 
-vector<double> input_numbers(size_t count);
-void find_minmax(vector<double> numbers, double& min, double& max);
-vector<size_t> make_histogram(vector<double>numbers, size_t bin_count);
-void show_histogram_text(vector<size_t> bins);
+void test_negative()//Случай массива отриц. чисел
+{
+    double min;
+    double max;
+    find_minmax({-1, -5,-228}, min, max);
+    assert(min == -228);
+    assert(max == -1);
+}
+
+void test_one_number()//массив из одного числа
+{
+    double min;
+    double max;
+    find_minmax({666}, min, max);
+    assert(min == 666);
+    assert(max == 666);
+}
+
+void test_same_numbers()//массив из трех одинаковых чисел
+{
+    double min;
+    double max;
+    find_minmax({3,3,3}, min, max);
+    assert(min == 3);
+    assert(max == 3);
+}
+
+void test_empty_vector()//пустой массив
+{
+    double min;
+    double max;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
+int main()
+{
+    test_positive();
+    test_negative();
+    test_one_number();
+    test_same_numbers();
+    test_empty_vector();
+}
