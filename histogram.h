3e0623bdf440ff53ee5956f7d33bebc49b21e3d2
111 <a@1>
2021-04-24 19:00:34 +0300

    comments for functions

--- histogram.h
+++ histogram.h
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-vector<double> input_numbers(size_t count);
-void find_minmax(vector<double> numbers, double& min, double& max);
-vector<size_t> make_histogram(vector<double>numbers, size_t bin_count);
-void show_histogram_text(vector<size_t> bins);
+vector<double> input_numbers(size_t count); // ввод значений
+void find_minmax(vector<double> numbers, double& min, double& max); // нахождение мин. и макс. значения
+vector<size_t> make_histogram(vector<double>numbers, size_t bin_count); // составляет гистограмму
+void show_histogram_text(vector<size_t> bins); // отображает гистограмму в текстовом виде
 
--- svg.h
+++ svg.h
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-void svg_begin(double width, double height);
-void svg_end();
-void svg_text(double left, double baseline, string text);
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fil = "black");
-void show_histogram_svg(const vector<size_t>& bins, size_t SHIRINA_BLOCKA);
+void svg_begin(double width, double height); // вывод загаловка
+void svg_end(); // окончание cvg
+void svg_text(double left, double baseline, string text); // вывод подписей к столбцам
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fil = "black"); // вывод прямоугольника в svg
+void show_histogram_svg(const vector<size_t>& bins, size_t SHIRINA_BLOCKA); // вывод гистограммы в svg
 
